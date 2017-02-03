5fd4d59739a7fa2c358743fcacdc622b816ae9c2
Нурсултан Алмаханов <nurik-1998kz@outlook.com>
2017-02-03 23:30:06 +0600

    Prime_Min

---
+++ Lab2_FileSystem_PrimeMin/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MAX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"D:\input.txt");
+
+            System.Console.WriteLine("Contents of Input.txt = " + "\n");
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            int mini = 1000000;
+
+            for (int i = 0; i < lines.Length; i++)
+
+            {
+                if (mini > int.Parse(lines[i]))
+                {
+                    mini = int.Parse(lines[i]);
+                }
+            }
+
+            File.WriteAllText(@"D:\output.txt", "the minimum prime number is: " + mini);
+
+            Console.WriteLine("\n" + "Min number is: " + mini + "\n");
+
+            Console.WriteLine("Press any key to exit.");
+            
+            System.Console.ReadKey();
+        }
+
+    }
+
+}
