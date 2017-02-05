344d2f1de91ea1f83baf71bbebf5adc72d730eae
Нурсултан Алмаханов <nurik-1998kz@outlook.com>
2017-02-05 22:41:41 +0600

    Far

---
+++ Lab3_FarManager/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_FarManager
+{
+    class Program
+    {
+        public static void ShowFolder(State state)
+        {
+            Console.Clear();
+            DirectoryInfo[] list = state.Folder.GetDirectories();
+            for(int i = 0; i<list.Length; i++)
+            {
+                if(i == state.Index)
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                }
+                Console.Write(list[i].Name);
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine();
+            }
+        }
+        
+
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Stack<State> layers = new Stack<State>();
+            bool alive = true;
+            State state = new State { Folder = new DirectoryInfo(@"C:\"), Index = 0 };
+            layers.Push(state);
+
+            while (alive)
+            {
+                ShowFolder(layers.Peek());
+                ConsoleKeyInfo pressedKey = Console.ReadKey();
+                switch (pressedKey.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        layers.Peek().Index--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        layers.Peek().Index++;
+                        break;
+                    case ConsoleKey.Backspace:
+                        layers.Pop();
+                        break;
+                    case ConsoleKey.Escape:
+                        alive = false;
+                        break;
+                    case ConsoleKey.Enter:
+                        DirectoryInfo f = layers.Peek().Folder.GetDirectories()[layers.Peek().Index];
+                        State substate = new State
+                        {
+                            Folder = f,
+                            Index = 0
+                        };
+                        layers.Push(substate);
+                        break;
+                    default:
+                        break;
+                }
+            }//end while
+        }//end Main
+    }//end class
+}
---
+++ Lab3_FarManager/State.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_FarManager
+{
+    class State
+    {
+        int index;
+        DirectoryInfo folder;
+        public int MaxIndex;
+
+        public DirectoryInfo Folder
+        {
+            get
+            {
+                return folder;
+            }
+            set
+            {
+                folder = value;
+                MaxIndex = folder.GetDirectories().Length;
+            }
+        }
+        public int Index
+        {
+            get
+            {
+                return index;
+            }
+            set
+            {
+                if(value >= 0 && value < MaxIndex)
+                {
+                    index = value;
+                }
+            }//set
+        }//end index
+        
+    }//end class
+}
